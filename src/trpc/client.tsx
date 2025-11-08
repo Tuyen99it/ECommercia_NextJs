8efca4eb32e80a4994b5b6082b6e22255ca03b1b
Tuyen99it <nguyentuyen99tnut@gmail.com>
2025-11-08 17:44:45 +0700

    Fix issue can not submit data to trpc server to not config supperjson at client side

--- src/trpc/client.tsx
+++ src/trpc/client.tsx
@@ -6,8 +6,10 @@ import { QueryClientProvider } from '@tanstack/react-query';
 import { createTRPCClient, httpBatchLink } from '@trpc/client';
 import { createTRPCContext } from '@trpc/tanstack-react-query';
 import { useState } from 'react';
-import { makeQueryClient } from './query-client';
+import superjson from "superjson"
 import type { AppRouter } from './routers/_app';
+import { makeQueryClient } from './query-client';
+
 
 export const { TRPCProvider, useTRPC } = createTRPCContext<AppRouter>();
 
@@ -46,7 +48,7 @@ export function TRPCReactProvider(
     createTRPCClient<AppRouter>({
       links: [
         httpBatchLink({
-          // transformer: superjson, <-- if you use a data transformer
+          transformer: superjson, //<-- if you use a data transformer
           url: getUrl(),
         }),
       ],
