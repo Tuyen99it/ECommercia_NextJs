18fdd239b4c9a8326dc0fbcb09afb695b51f7086
Tuyen99it <nguyentuyen99tnut@gmail.com>
2025-11-08 17:24:41 +0700

    fix: config use superjson at client

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -7,9 +7,9 @@ import { buildConfig } from 'payload'
 import { fileURLToPath } from 'url'
 import sharp from 'sharp'
 
-import { Users } from './collections/Users.js'
-import  Media  from './collections/Media.js'
-import Categories from "./collections/Categories.js"
+import { Users } from './collections/Users.ts'
+import  Media  from './collections/Media.ts'
+import Categories from "./collections/Categories.ts"
 const filename = fileURLToPath(import.meta.url)
 const dirname = path.dirname(filename)
 
--- src/trpc/client.tsx
+++ src/trpc/client.tsx
@@ -7,6 +7,7 @@ import { createTRPCContext } from '@trpc/tanstack-react-query';
 import { useState } from 'react';
 import { makeQueryClient } from './query-client';
 import type { AppRouter } from './routers/_app';
+import superjson from "superjson";
 export const { TRPCProvider, useTRPC } = createTRPCContext<AppRouter>();
 let browserQueryClient: QueryClient;
 function getQueryClient() {
@@ -41,9 +42,10 @@ export function TRPCReactProvider(
   const queryClient = getQueryClient();
   const [trpcClient] = useState(() =>
     createTRPCClient<AppRouter>({
+  
       links: [
         httpBatchLink({
-          // transformer: superjson, <-- if you use a data transformer
+          transformer: superjson, //<-- if you use a data transformer
           url: getUrl(),
         }),
       ],
